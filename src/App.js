46d35cb76cc89d3889601485d3647a9a06dc6a7b
Alya <obarlanytska@gmail.com>
2021-12-04 20:47:11 +0200

    commit todo-list

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  Input  from "./components/Input";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className='container'>
+      <Input />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/SimpleButton.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import Button from "@mui/material/Button";
+
+
+export const SimpleButton = ({ text, onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      {text}
+    </Button>
+  );
+};
+
+SimpleButton.prototypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
