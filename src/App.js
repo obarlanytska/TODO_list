5dce379adf14c6333bc6f634995823d4fe7f8c09
Alya <obarlanytska@gmail.com>
2021-12-31 15:22:20 +0200

    Revert "split project to components"
    This reverts commit 3f05a98970c9434ffc384617b30e357c1ac0ec44.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  TodoElement  from "./components/TodoElement";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className='container'>
+      <TodoElement />
+    </div>
+  );
+}
+export default App;
